53442bbad3c16e3183373eaa491c13f53f0c3362
Anand <147962898+nisha1289@users.noreply.github.com>
2024-02-03 09:27:30 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 26435700
-api_hash = "527cf5174e120a9093611bc69d7b7709"
-bot_token = "6180767554:AAGKw-Eo6L0DXlL4BsrDmKItUewgdKGAtGo"
+api_id = 27039595
+api_hash = "aabe9b61bbdb73ea4b35fc4faa880621"
+bot_token = "6857799966:AAEcIbjlwlumfZ3_3REfoJyl7q3w_UjSnUQ"
 
